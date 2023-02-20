ccb1ba1f2d5c439430fdb8c56995d2ba48d409be
Vishal Vansjariya <vishalvansjariya2512@gmail.com>
2023-02-20 06:26:30 +0530

    initial

---
+++ README.md
@@ -0,0 +1,8 @@
+# Results Summary Component
+
+![design](./design.jpg)
+
+## Frontend Mentor Challenge
+
+📘 [See Challenge](https://www.frontendmentor.io/challenges/results-summary-component-CE_K6s0maV)</br>
+🚀 [See Solution](https://vishal-results-summary-component.netlify.app/)
---
+++ script.js
@@ -0,0 +1,23 @@
+const totalScore = document.querySelector("#totalScore");
+const reactionScore = document.querySelector("#reactionScore");
+const memoryScore = document.querySelector("#memoryScore");
+const verbalScore = document.querySelector("#verbalScore");
+const visualScore = document.querySelector("#visualScore");
+
+const updateScore = (element, score, time) => {
+  let count = 0;
+  setInterval(() => {
+    element.textContent = count;
+    if (count < score) {
+      count++;
+    }
+  }, time);
+};
+
+window.addEventListener("load", () => {
+  updateScore(totalScore, 76, 42);
+  updateScore(reactionScore, 80, 32);
+  updateScore(memoryScore, 92, 30);
+  updateScore(verbalScore, 61, 38);
+  updateScore(visualScore, 72, 35);
+});
